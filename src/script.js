b9265df9235271d34585885edfba8f0fe50aac89
Kade <kadeesterline@gmail.com>
2022-05-19 15:16:29 -0700

    adds typedjs & styling

---
+++ src/script.js
@@ -0,0 +1,15 @@
+let options = {
+  strings: [
+    "Software Engineer",
+    "Problem Solver",
+    "Frontend Developer",
+    "Freelancer",
+  ],
+  shuffle: true,
+  showCursor: false,
+  loop: true,
+  typeSpeed: 60,
+  backSpeed: 60,
+};
+
+let typed = new Typed("#intro-span", options);
