823d5a1fad4b25c4917aa1bb193cb34ec3afbb49
Kade <kadeesterline@gmail.com>
2022-07-19 11:45:04 -0700

    adds basic home page

--- portfolio/src/Components/NavBar.jsx
+++ portfolio/src/Components/NavBar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <header class="header">
+      <a class="header-link" href="#contacts">
+        Contact
+      </a>
+      <a class="header-link" href="#skills">
+        Skills
+      </a>
+      <a class="header-link" href="#projects">
+        Projects
+      </a>
+    </header>
+  );
 }
 
 export default NavBar;
--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -1,7 +1,54 @@
 import React from "react";
+import TypeWriterEffect from "react-typewriter-effect";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div id="intro">
+        <div class="intro-container">
+          <div class="intro-wrapper">
+            <div class="intro-text">
+              <div>
+                <h1 class="intro-header">
+                  Hi, I'm Kade
+                  <TypeWriterEffect
+                    textStyle={{
+                      fontWeight: 550,
+                      fontSize: "1em",
+                    }}
+                    startDelay={2000}
+                    multiText={[
+                      "a Freelancer",
+                      "a Problem Solver",
+                      "a Frontend Developer",
+                      "a Software Engineer",
+                    ]}
+                    multiTextDelay={1000}
+                    typeSpeed={30}
+                  />
+                  <span id="intro-span"></span>
+                </h1>
+              </div>
+              <div>
+                <h1 class="intro-paragraph">
+                  Welcome to my portfolio! You'll find descriptions and links to
+                  projects, blog posts, skills and how you can contact me using
+                  the links in the navbar.
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="wave-wrapper wave-1">
+        <img
+          class="wave"
+          src="./src/images/stacked-waves-haikei.svg"
+          alt="wave"
+        />
+      </div>
+    </>
+  );
 }
 
 export default Home;
